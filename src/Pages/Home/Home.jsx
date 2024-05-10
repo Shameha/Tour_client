ebf2cd11bf48a4d3ce124ee2b0649a51bf8d8ce7
shamehaera9 <shamehaera9@gmail.com>
2024-05-10 12:56:48 +0600

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+import Banner from "../Banner/Banner";
+
+const Home = () => {
+    return (
+        <div>
+      <div className="mb-2">
+         <Banner></Banner>
+           </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+// import React from 'react';
+import logo from "../../assets/istockphoto-931069122-612x612.jpg"
+
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+
+    const navLink = <>
+
+    <li><NavLink to="/">Home</NavLink></li>
+    <li><NavLink to="/volunteer">Need Volunteer</NavLink></li>
+        {/* <li><NavLink to="/addedTour">Add Tourists Spot</NavLink></li> */}
+      
+        {/* <li><NavLink to="/list">My List</NavLink></li> */}
+    
+      </>
+    return (
+        <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              {navLink}
+            </ul>
+          </div>
+          <img src={logo} width={50} height={50} alt="" />
+    <a className=" lg:text-xl md:text-sm text-sm font-bold ">UnityServe</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            {navLink}
+          </ul>
+        </div>
+        <div className="navbar-end">
+        <Link to="/login"><button className="btn ">Login</button></Link>
+        </div>
+      </div>
+    );
+};
+
+export default Navbar;
