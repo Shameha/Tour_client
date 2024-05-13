76ed5c64452bb4265e94b1c2e8a4181da91ed193
shamehaera9 <shamehaera9@gmail.com>
2024-05-13 21:56:43 +0600

    added axios

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -59,17 +59,28 @@ const logOut = () =>{
 
 useEffect(() =>{
 const unsubscribe = onAuthStateChanged(auth,currentUser=>{
+   const userEmail = currentUser?.email || user?.email;
+   const loggedUser = {email: userEmail};
     setUser(currentUser);
     console.log("current user",currentUser)
     setLoading(false)
     if(currentUser){
-    const loggedUser = {email: currentUser.email}
+   
      axios.post('http://localhost:5000/jwt',loggedUser,{withCredentials:true})
      .then(res =>{
         console.log('token response',res.data);
      })
     }
-})
+    else{
+     axios.post('http://localhost:5000/logout',loggedUser,{
+        withCredentials:true
+     })
+     .then(res=>{
+        console.log(res.data);
+     })
+    }
+
+});
 return()=>{
     return unsubscribe();
 }
