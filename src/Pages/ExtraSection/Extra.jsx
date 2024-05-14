24fbb0f5fccbe0f46936a4c520c847794e8b43fb
shamehaera9 <shamehaera9@gmail.com>
2024-05-14 13:44:29 +0600

    added

--- src/Pages/ExtraSection/Extra.jsx
+++ src/Pages/ExtraSection/Extra.jsx
@@ -10,8 +10,8 @@ const Extra = () => {
 		</div>
 		<div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
 			<div>
-				<h3 className="text-2xl font-bold tracking-tight sm:text-3xl dark:text-gray-900">Ad vix debet docendi</h3>
-				<p className="mt-3 text-lg dark:text-gray-600">Ne dicta praesent ocurreret has, diam theophrastus at pro. Eos etiam regione ut, persius eripuit quo id. Sit te euismod tacimates.</p>
+				<h3 className="text-2xl font-bold tracking-tight sm:text-3xl dark:text-gray-900">Types of Volunteering</h3>
+				<p className="mt-3 text-lg dark:text-gray-600"> Direct service: Hands-on involvement in activities like tutoring, building homes, or serving meals at shelters.</p>
 				<div className="mt-12 space-y-12">
 					<div className="flex">
 						<div className="flex-shrink-0">
@@ -22,8 +22,8 @@ const Extra = () => {
 							</div>
 						</div>
 						<div className="ml-4">
-							<h4 className="text-lg font-medium leading-6 dark:text-gray-900">Per ei quaeque sensibus</h4>
-							<p className="mt-2 dark:text-gray-600">Ex usu illum iudico molestie. Pro ne agam facete mediocritatem, ridens labore facete mea ei. Pro id apeirian dignissim.</p>
+							<h4 className="text-lg font-medium leading-6 dark:text-gray-900">Benefits of Volunteering</h4>
+							<p className="mt-2 dark:text-gray-600">Personal fulfillment: Volunteering offers a sense of purpose, satisfaction, and fulfillment by making a positive impact on others lives.</p>
 						</div>
 					</div>
 					<div className="flex">
@@ -35,8 +35,8 @@ const Extra = () => {
 							</div>
 						</div>
 						<div className="ml-4">
-							<h4 className="text-lg font-medium leading-6 dark:text-gray-900">Cu imperdiet posidonium sed</h4>
-							<p className="mt-2 dark:text-gray-600">Amet utinam aliquando ut mea, malis admodum ocurreret nec et, elit tibique cu nec. Nec ex maluisset inciderint, ex quis.</p>
+							<h4 className="text-lg font-medium leading-6 dark:text-gray-900">Volunteer Opportunities</h4>
+							<p className="mt-2 dark:text-gray-600">Community service organizations: Nonprofits, charities, and community centers focused on addressing local needs such as hunger, homelessness, education, and healthcare.</p>
 						</div>
 					</div>
 					<div className="flex">
@@ -48,8 +48,8 @@ const Extra = () => {
 							</div>
 						</div>
 						<div className="ml-4">
-							<h4 className="text-lg font-medium leading-6 dark:text-gray-900">Nulla omittam sadipscing mel ne</h4>
-							<p className="mt-2 dark:text-gray-600">At sed possim oporteat probatus, justo graece ne nec, minim commodo legimus ut vix. Ut eos iudico quando soleat, nam modus.</p>
+							<h4 className="text-lg font-medium leading-6 dark:text-gray-900">Volunteerism Worldwide</h4>
+							<p className="mt-2 dark:text-gray-600">Volunteerism is a global phenomenon, with individuals and organizations contributing to societal well-being across borders and cultures.</p>
 						</div>
 					</div>
 				</div>
@@ -61,7 +61,7 @@ const Extra = () => {
 		<div>
 			<div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
 				<div className="lg:col-start-2">
-					<h3 className="text-2xl font-bold tracking-tight sm:text-3xl dark:text-gray-900">Eam nibh gloriatur ex</h3>
+					<h3 className="text-2xl font-bold tracking-tight sm:text-3xl dark:text-gray-900">Recognition and Support</h3>
 					<p className="mt-3 text-lg dark:text-gray-600">Per odio fabellas consulatu cu. Utroque detracto mel ea, quo te latine theophrastus. Ea his tale nibh dissentias, mei exerci tamquam euripidis cu.</p>
 					<div className="mt-12 space-y-12">
 						<div className="flex">
@@ -73,7 +73,7 @@ const Extra = () => {
 								</div>
 							</div>
 							<div className="ml-4">
-								<h4 className="text-lg font-medium leading-6 dark:text-gray-900">Cibo augue offendit has ad</h4>
+								<h4 className="text-lg font-medium leading-6 dark:text-gray-900">Volunteer Diversity</h4>
 								<p className="mt-2 dark:text-gray-600">An per velit appellantur, ut utinam minimum nominavi sit, odio nostro habemus ne nec. Ne sonet regione contentiones est.</p>
 							</div>
 						</div>
@@ -86,7 +86,7 @@ const Extra = () => {
 								</div>
 							</div>
 							<div className="ml-4">
-								<h4 className="text-lg font-medium leading-6 dark:text-gray-900">At eum ferri luptatum lobortis</h4>
+								<h4 className="text-lg font-medium leading-6 dark:text-gray-900">Flexible Commitments</h4>
 								<p className="mt-2 dark:text-gray-600">Te per quidam maiorum ocurreret, etiam delicatissimi usu ad. Ne has quod periculis. Te sit primis iisque efficiantur.</p>
 							</div>
 						</div>
@@ -99,7 +99,7 @@ const Extra = () => {
 								</div>
 							</div>
 							<div className="ml-4">
-								<h4 className="text-lg font-medium leading-6 dark:text-gray-900">Dicunt verterem evertitur eu sea</h4>
+								<h4 className="text-lg font-medium leading-6 dark:text-gray-900">Youth Engagement</h4>
 								<p className="mt-2 dark:text-gray-600">Audire principes rationibus eam an, autem nominavi luptatum per te. Sumo fabulas vim eu, sonet saperet eleifend ut vix.</p>
 							</div>
 						</div>
